6f857a50c4fbc6d4db0215c183e6afe2ff577381
mraidi achref <mradiachref@gmail.com>
2024-02-18 00:43:16 +0100

    done with crud function

--- src/anime/anime.controller.js
+++ src/anime/anime.controller.js
@@ -7,6 +7,25 @@ exports.create = async (req, res) => {
 };
 
 exports.delete = async (req, res) => {
-  await AnimeModel.deleteOne({uid:req.body.uid});
+  await AnimeModel.deleteOne({ uid: req.body.uid });
   return res.sendStatus(204);
+};
+
+exports.update = async (req, res) => {
+  const updateResult = await AnimeModel.updateOne({ uid: req.body.uid },req.body );
+  if (updateResult.modifiedCount === 0) {
+    return res.sendStatus(404); // Not Found, if no document is updated
+  }
+  return res.sendStatus(200); // OK, document updated successfully
+};
+
+
+exports.read = async (req, res) => {
+    const anime = await AnimeModel.findOne({ uid: req.params.uid });
+    console.log(anime) ;
+    if (!anime) {
+      return res.sendStatus(404); // Not Found, if the document does not exist
+    }
+    return res.status(200).json(anime).json; // OK, return the document data
+
 };
--- src/anime/anime.routes.js
+++ src/anime/anime.routes.js
@@ -7,4 +7,8 @@ router.post('/create-anime',animeController.create  );
 
 router.post('/delete-anime',animeController.delete  );
 
+router.post('/update-anime',animeController.update  );
+
+router.get('/read-anime/:uid',animeController.read  );
+
 module.exports = router ;
