0729b217828e2d21f2db78e63766b3b0b4852da2
mraidi achref <mradiachref@gmail.com>
2024-02-18 22:15:34 +0100

    profiles crud completed

--- src/profiles/profiles.controller.js
+++ src/profiles/profiles.controller.js
@@ -9,4 +9,37 @@ exports.delete = async (req, res) => {
     await ProfilesModel.deleteOne({profile : req.body.profile});
     return res.sendStatus(204);
   };
+
+
+  exports.update = async (req, res) => {
+    const updateResult = await ProfilesModel.updateOne(
+      { profile: req.body.profile },
+      req.body
+    );
+    if (updateResult.modifiedCount === 0) {
+      return res.sendStatus(404); // Not Found 
+    }
+    return res.sendStatus(200); //  successfully
+  };
+
+
+
+  exports.read = async (req, res) => {
+    const profile = await ProfilesModel.findOne({ profile: req.params.profile });
+    if (!profile) {
+      return res.sendStatus(404); 
+    }
+    return res.status(200).json(profile); 
+  };
   
+
+  exports.readAllByGender = async (req, res) => {
+    const profileList = await ProfilesModel.find({ gender: req.params.gender });
+  
+   
+    if (profileList.length === 0) {
+      return res.sendStatus(404); 
+    }
+  
+    return res.status(200).json(profileList); 
+  };
--- src/profiles/profiles.routes.js
+++ src/profiles/profiles.routes.js
@@ -8,5 +8,12 @@ router.post('/create-profiles',profilesController.create  );
 router.post('/delete-profiles',profilesController.delete  );
 
 
+router.post('/update-profiles',profilesController.update  );
+
+router.get('/read-profiles/profile/:profile',profilesController.read );
+
+router.get('/readByGender-profiles/gender/:gender',profilesController.readAllByGender );
+
+
 
 module.exports = router ;
